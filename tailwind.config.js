696770420bb788f1cc5074e86aaf4424d2e2aee7
Caleb <benJephunneh@users.noreply.github.com>
2022-06-20 12:57:14 -0400

    Restructure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 module.exports = {
   mode: 'jit',
   content: [
-    "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
