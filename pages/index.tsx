2bd7fb25c1e7b57027b620e28d7d402cdf6b6ebe
Caleb <benJephunneh@users.noreply.github.com>
2022-06-20 12:45:41 -0400

    Add basic modal structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex flex-col">
-      <div className="bg-red-200">First</div>
-      <div className="bg-pink-400">Second</div>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which?</div>
+      <div className="p-2"/>
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-blue-200"></div>
+      </div>
     </div>
   );
 }
